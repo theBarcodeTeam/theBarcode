78febc002e3db957296d5c79ff447f9992cb8c2e
goodpineapple <taemin3000@naver.com>
2019-02-16 18:40:02 +0900

    change images

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -16,48 +16,6 @@ $(document).ready(function(){
     });
   });
 
-
-  //#####################################################//
-  //###################### parallex #####################//
-  //####################################################//
-  var scene_vocal = $('#scene_vocal')[0];
-  var parallaxInstance_vocal = new Parallax(scene_vocal, {
-    hoverOnly : true
-  });
-  parallaxInstance_vocal.friction(0.2, 0.2);
-
-  var scene_keyboard = $('#scene_keyboard')[0];
-  var parallaxInstance_keyboard = new Parallax(scene_keyboard, {
-    hoverOnly : true
-  });
-  parallaxInstance_keyboard.friction(0.2, 0.2);
-
-  var scene_guitar = $('#scene_guitar')[0];
-  var parallaxInstance_guitar = new Parallax(scene_guitar, {
-    hoverOnly : true
-  });
-  parallaxInstance_guitar.friction(0.2, 0.2);
-
-  var scene_drum = $('#scene_drum')[0];
-  var parallaxInstance_drum = new Parallax(scene_drum, {
-    hoverOnly : true
-  });
-  parallaxInstance_drum.friction(0.2, 0.2);
-
-  var scene_bass = $('#scene_bass')[0];
-  var parallaxInstance_bass = new Parallax(scene_bass, {
-    hoverOnly : true
-  });
-  parallaxInstance_bass.friction(0.2, 0.2);
-
-  var parallax_container = document.getElementById('parallax_container');
-  var parallaxInstance = new Parallax(parallax_container, {
-    calibrateX : true,
-    calibrateY : false
-  });
-  parallaxInstance.friction(0.2, 0.2);
-
-
   ovf = $(".overflow")[0]
   slider = $(".scroll_hider")[0]
   winResize();
